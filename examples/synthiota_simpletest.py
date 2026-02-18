74184008bb9278ee9e158da1f583fffb6c55cf5a
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-02-18 11:53:10 -0600

    Improve simpletest

--- examples/synthiota_simpletest.py
+++ examples/synthiota_simpletest.py
@@ -24,21 +24,25 @@ palette[0] = 0xFFFFFF
 root_group.append(
     vectorio.Circle(
         pixel_shader=palette,
-        radius=synthiota.display.width // 2,
+        radius=synthiota.display.height // 2,
         x=synthiota.display.width // 2,
         y=synthiota.display.height // 2,
     )
 )
 
 # audio
-synth = synthio.Synthesizer(synthiota.sample_rate)
+synth = synthio.Synthesizer(sample_rate=synthiota.sample_rate)
 synth.envelope = synthio.Envelope(attack_time=0.05, attack_level=0.8, release_time=0.6)
 synthiota.mixer.voice[0].play(synth)
 synthiota.mixer.voice[0].level = 0.25  # 0.25 usually better for headphones, 1.0 for line-in
 
 notenum = None
 note_counter = 0
+control_counter = 0
 last_timestamp = time.monotonic()
+encoder_pressed = False
+octave_up_pressed = False
+octave_down_pressed = False
 while True:
     current_timestamp = time.monotonic()
     delta = current_timestamp - last_timestamp
@@ -57,12 +61,23 @@ while True:
             note_counter = 0.5
 
     # controls
-    print("Encoder Position:", synthiota.encoder_position)
-    print("Encoder Switch:", synthiota.encoder_button.value)
-    print("Octave Up:", synthiota.octave_up_button.value)
-    print("Octave Down:", synthiota.octave_up_button.value)
-    print("Left Slider:", synthiota.left_slider.value)
-    print("Right Slider:", synthiota.right_slider.value)
-    print("Steps:", "".join([str(int(x)) for x in synthiota.touched_steps]))
+    if (control_counter := control_counter - delta) <= 0:
+        print("Encoder Position:", synthiota.encoder_position)
+        print("Encoder Switch:", encoder_pressed)
+        print("Octave Up:", octave_up_pressed)
+        print("Octave Down:", octave_down_pressed)
+        print("Left Slider:", synthiota.left_slider.value)
+        print("Right Slider:", synthiota.right_slider.value)
+        print("Steps:", "".join([str(int(x)) for x in synthiota.touched_steps]))
+        print()
+        encoder_pressed = False
+        octave_up_pressed = False
+        octave_down_pressed = False
+        control_counter = 0.1
 
-    time.sleep(0.1)
+    synthiota.update_buttons()
+    encoder_pressed = encoder_pressed or synthiota.encoder_button.pressed
+    octave_up_pressed = octave_up_pressed or synthiota.octave_up_button.pressed
+    octave_down_pressed = octave_down_pressed or synthiota.octave_down_button.pressed
+
+    last_timestamp = current_timestamp
